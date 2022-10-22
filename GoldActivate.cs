03aa0b39ccbf58221c9431bf05b720d458eaadda
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2022-10-22 16:08:49 +0400

    Update and rename TakeGold.cs to GoldActivate.cs

--- GoldActivate.cs
+++ GoldActivate.cs
@@ -1,14 +1,17 @@
-public class TakeGold : MonoBehaviour
+public class GoldActivate : MonoBehaviour
 {
     [SerializeField] private UnityEvent _reached;
 
+    private float _destroyGold = 0.5f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.TryGetComponent<Player>(out Player player))
         {
             gameObject.SetActive(true);
             _reached?.Invoke();
-            Destroy(gameObject,0.5f);
+            Destroy(gameObject, _destroyGold);
         }
     }
 }
