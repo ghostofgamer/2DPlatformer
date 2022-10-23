7173cd2c1ef3c38614e6590ec39a8f1ba44461f0
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2022-10-23 12:50:15 +0400

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -6,26 +6,29 @@ public class Movement : MonoBehaviour
     [SerializeField] private Rigidbody2D _rigidbody2D;
     [SerializeField] private float _jumpForce;
 
+    const string Run = "PlayerRun";
+    const string Jump = "PlayerJump";
+
     private void Update()
     {
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(_speed * Time.deltaTime, 0, 0);
-            _animation.Play("PlayerRun");
+            _animation.Play(Run);
             _renderer.flipX = false;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
-            _animation.Play("PlayerRun");
+            _animation.Play(Run);
             _renderer.flipX = true;
         }
 
         if (Input.GetKey(KeyCode.Space ))
         {
             _rigidbody2D.AddForce(Vector2.up * _jumpForce);
-            _animation.Play("PlayerJump");
+            _animation.Play(Jump);
         }
     }
 }
